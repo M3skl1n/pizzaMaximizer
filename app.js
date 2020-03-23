1b304ac45dcb210e7d8622eee4077563e109a575
John Cornia <john.cornia.1@gmail.com>
2020-03-23 09:13:23 -0600

    trying to include now the File System Module

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 const port = process.env.PORT || 3000
 
-    const server = http.createServer((req, res) => {
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
+
+    /*const server = http.createServer((req, res) => {
 	    res.statusCode = 200;
 	    res.setHeader('Content-Type', 'text/html');
 	    res.end('<h1>Hello World</h1>');
@@ -10,4 +20,4 @@ const port = process.env.PORT || 3000
 
 server.listen(port,() => {
 	console.log(`Server running at port `+port);
-    });
+    });*/
