9a4d3183397320e0b4d5267284918f3234ff592d
John Cornia <john.cornia.1@gmail.com>
2020-03-23 09:21:19 -0600

    restoring

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 const http = require('http');
 const port = process.env.PORT || 3000
 
-const server = http.createServer(function (req, res) => {
-    fs.readFile('demofile1.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-       });
-    });
-
-server.listen(port,() => {
-    console.log(`Server running at port `+port);
-    });
-
-    /*const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
 	    res.statusCode = 200;
 	    res.setHeader('Content-Type', 'text/html');
 	    res.end('<h1>Hello World</h1>');
@@ -22,4 +10,4 @@ server.listen(port,() => {
 
 server.listen(port,() => {
 	console.log(`Server running at port `+port);
-    });*/
+    });
