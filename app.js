423f814903b75e4fa16fa92f9cf8e685008717d3
John Cornia <john.cornia.1@gmail.com>
2020-03-23 09:19:59 -0600

    still working on file system module

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const port = process.env.PORT || 3000
 
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
+const server = http.createServer(function (req, res) => {
+    fs.readFile('demofile1.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+       });
+    });
+
+server.listen(port,() => {
+    console.log(`Server running at port `+port);
+    });
 
     /*const server = http.createServer((req, res) => {
 	    res.statusCode = 200;
