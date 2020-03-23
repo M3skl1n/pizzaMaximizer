e6782c8f566738b09c684fba88758387b849a5a4
John Cornia <john.cornia.1@gmail.com>
2020-03-23 09:28:43 -0600

    think I got it

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
 const port = process.env.PORT || 3000
+var fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	    res.statusCode = 200;
-	    res.setHeader('Content-Type', 'text/html');
-	    res.end('<h1>Hello World</h1>');
-                                     
+        fs.readFile('demofile1.html', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          res.end();
+        });
 	});
 
 server.listen(port,() => {
 	console.log(`Server running at port `+port);
     });
+
+/*
+ res.statusCode = 200;
+ res.setHeader('Content-Type', 'text/html');
+ res.end('<h1>Hello World</h1>');
+ */
